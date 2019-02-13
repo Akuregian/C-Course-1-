f0d3adcfcb5f98b39970e1dcd15d4532362b0812
Anthony Kuregian <Anthonykuregian@yahoo.com>
2019-02-12 16:49:02 -0800

    added some extra lines of code to the main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
 
 int main() 
 {
-    std::string name;
-    std::cout << "Whats your name ";
+    std::string name, age;
 
+    std::cout << "Hey Their Little Feller, Whats your name?" << endl;
+    getline (cin, name);
+    std::cout << "My Dogs name was " << name << "... uhh well.. hmm." << endl;
+    std::cout << "Sorry about that, Im getting old.. How old are you?" << endl;
+    getline (cin, age);
+    std::cout << "Wow.. Your fucking old.. Holy shit. I never new someone could reach " << age << ".";
+    return 0;
 }
