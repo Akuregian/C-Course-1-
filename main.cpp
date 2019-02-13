ae11e7bad79bf94bc886779586ca39380083f170
Anthony Kuregian <Anthonykuregian@yahoo.com>
2019-02-12 17:13:06 -0800

    Calcultor

--- main.cpp
+++ main.cpp
@@ -1,17 +1,67 @@
-#include <iostream>
-#include <string>
-
+#include<iostream>
+#include<cmath>
+ 
 using namespace std;
-
-int main() 
+ 
+int main()
 {
-    std::string name, age;
+    double num1,num2;
+    char operation,redo;
+    cout <<"Welcome to the calculater program v.1.0 written by Anthony Kuregian" << endl;
+    cout <<"***************************************************************"<<endl;
+    cout << endl << endl << endl;
 
-    std::cout << "Hey Their Little Feller, Whats your name?" << endl;
-    getline (cin, name);
-    std::cout << "My Dogs name was " << name << "... uhh well.. hmm." << endl;
-    std::cout << "Sorry about that, Im getting old.. How old are you?" << endl;
-    getline (cin, age);
-    std::cout << "Wow.. Your fucking old.. Holy shit. I never new someone could reach " << age << ".";
+    do
+    {
+        cout << " Please enter an operation which you like to calculate (+,-,*,/,s)";
+        cout << "[s stands for swap]:";
+        cin >> operation ;
+        cout << endl << endl;
+        cout <<" Please enter two numbers to apply your requested operation(";
+        cout << operation<<"):" << endl << "1st num:";
+        cin >> num1;
+        cout << "2nd num:" ;
+        cin >> num2;
+        cout << endl;
+        switch (operation)
+        {
+
+        case'+':
+            cout << "The addition of two numbers is ";
+            cout << num1+num2 << endl;
+            break;
+        case'-':
+            cout << "The substraction of two numbers (" << num1<<"," << num2 << "):";
+            cout << num1-num2<<endl;
+            break;
+        case'*':
+            cout << "The multiplication of two numbers (" << num1<<"," << num2 << "):";
+            cout << num1*num2<<endl;
+            break;
+        case'/':
+            cout<<"The division of two numbers ("<<num1<<","<<num2<<"):";
+            if(num2==0)
+            {
+                cout<<"not valid"<<endl;
+            }
+            cout<<(num1/num2)<<endl;
+            break;
+        case's':
+            cout<<"The swap of two numbers ("<<num1<<","<<num2<<"):";
+            swap(num1,num2);
+            cout<<"1stnumber="<<num1<<"and 2nd number="<<num2<<endl<<endl;
+            break;
+        default:
+            cout<<"unknown command"<<endl;
+ 
+        }
+        //----now once again the program will ask the user if want to continue or not
+        cout<<"enter y or Y to continue:";
+        cin>>redo;
+        cout<<endl<<endl;
+    }
+    while(redo=='y'||redo=='Y');
+ 
     return 0;
+ 
 }
