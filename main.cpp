2ba6d6da857f93db2958a5ec8568b4eb806e400f
Anthony Kuregian <Anthonykuregian@yahoo.com>
2019-02-14 20:44:38 -0800

    New

--- main.cpp
+++ main.cpp
@@ -5,7 +5,23 @@ using namespace std;
  
 int main()
 {
-    cout << "Deleted all the fooking code.. Cause I copied it."
+    float num1, num2, num3;
+
+    cout << "Enter 3 numbers and I can guess the largest number you input" << endl;
+    cout << "The First number is " << endl;
+    cin >> num1;
+    cout << "The second number is " << endl;
+    cin >> num2;
+    cout << "The third number is ";
+    cin >> num3;
+
+    if ((num1 >= num2) && (num1 >= num3))
+        cout << "The Largest number is " << num1 << endl;
+    if ((num2 >= num1) && (num2 >= num3))
+        cout << "The Largest number is " << num2 << endl;
+    if ((num3 >= num2) && (num3 >= num1))
+        cout << "The Largest Number is " << num3;
+
     return 0;
  
 }
