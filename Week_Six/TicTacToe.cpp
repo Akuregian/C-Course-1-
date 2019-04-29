bf569ade75dd7c483e36533e0cc619c27e4a63c3
Anthony Kuregian <Anthonykuregian@yahoo.com>
2019-04-28 20:09:30 -0700

    I think its finished...

--- Week_Six/TicTacToe.cpp
+++ Week_Six/TicTacToe.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <iomanip> 
-#include <string.h>
+#include <string>
 #include <typeinfo>
 
 using namespace std;
@@ -21,7 +21,12 @@ void DrawBaord(string matrix[3][3], int size)
     cout << endl << endl;
     }
 }
-
+/*
+Function updateBoard updates and then displays the new board and its new array
+@param matrix is the matrix being passed in
+@param size is the size of the matrix
+@param nextMove is the nextMove thats gathered from the userInput Function
+*/
 void updateBaord(string matrix[3][3], int size, int nextMove) 
 {
     int i = (nextMove - 1) / 3;
@@ -38,13 +43,44 @@ void updateBaord(string matrix[3][3], int size, int nextMove)
     cout << endl << endl;
     }
 }
-int userInput()
+/*
+Function will display the current player and then take in the user input
+If the user input is between 1 - 9 it will return the players move, else it will continue to recieve input
+*/
+int userInput(string matrix[3][3])
 {
-    int playerMove;
-    cin >> playerMove;
-    return playerMove;
+    if(player == 'X') {
+        cout << "Player X Turn: ";
+    }
+    else{
+        cout << "Player O Turn: ";
+    }
+
+    // Check and make sure input is valid
+    bool valid = false;
+    int flag = 0;
+    while (!valid) {
+        int playerMove;
+        cin >> playerMove;
+        int i = (playerMove - 1) / 3;
+        int j = (playerMove - 1) % 3;
+
+
+        if(playerMove > 0 && playerMove < 10 && matrix[i][j] != "X" && matrix[i][j] != "O") {
+                return playerMove;
+            }
+        else {
+            cout << "Invalid, try Again";
+        }
+    }
 }
 
+/*
+Function winnerCheck Checks to see if a winner has been found by checking each row, column and diagonal
+@param string matrix[][] is the matrix being passed in
+@param size is the size of the matrix
+@return will return a boolean value to determine if a winner has ben found. true == yes, false == no
+*/
 bool winnerCheck(string matrix[3][3], int size) 
 {
     bool winner = false;
@@ -75,37 +111,58 @@ bool winnerCheck(string matrix[3][3], int size)
         winner = true;
         return winner;
     }
-
     return winner;
 }
 
+/*
+Function that checks if all moves have been made and no more are left
+@param matrix[3][3] passes the matrix into the function
+@param size is the size of the corresponding matrix
+@return will return false if there are moves left to make and true when its a tie game
+*/
+
+int tieGameChecker(int count, int size) 
+{
+    if(count == size*size){
+        cout << "Time Game!" << endl;
+        exit(EXIT_FAILURE);
+    }
+    else {
+        count++;
+        cout << count << endl;
+        return count;
+    }
+}
+
+/*
+Function togglePlayer will flip the current player, so the other player can input
+*/
 void togglePlayer()
 {
     if(player == 'X'){
-        cout << "Player O Turn: ";
         player = 'O';
     }
     else {
-        cout << "Player X Turn: "; 
         player = 'X';
     }
 }
 
 int main()
-{
-    const int size = 3;
+{ 
+    const int size = 3; // Increase Size for Scaling
     string matrix[size][size] = { {"1", "2", "3"}, 
                                   {"4", "5", "6"}, 
                                   {"7", "8", "9"} };
     cout << endl;
     bool winner = false;
+    int counter = 1;
 
     // Function that displays the board
     DrawBaord(matrix, size);
 
     while (!winner) {
         // Function takes in user input
-        int nextMove = userInput();
+        int nextMove = userInput(matrix);
 
         // Draw the updated board
         updateBaord(matrix, size, nextMove);
@@ -114,13 +171,13 @@ int main()
         bool found = winnerCheck(matrix, size);
         
         if(found){
-            cout << "We Have A WINNNNNER" << endl;
-        }
-        else{
-            cout << "no Winner yet" << endl;
+            cout << "Player " << player << " has won the game!" << endl;
+            exit(EXIT_FAILURE);
         }
 
         // Function that checks if all slots are filled
+        counter = tieGameChecker(counter, size);
+
 
         // Flip players
         togglePlayer();
