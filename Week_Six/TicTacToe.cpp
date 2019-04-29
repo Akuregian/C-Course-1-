234e3e9cbe2b421e4a19b9850f0a4607ae4bb924
Anthony <AnthonyKuregian@yahoo.com>
2019-04-28 18:09:18 -0700

    Progress in the Tic Tac Toe Game

--- Week_Six/TicTacToe.cpp
+++ Week_Six/TicTacToe.cpp
@@ -1,51 +1,132 @@
 #include <iostream>
+#include <iomanip> 
+#include <string.h>
+#include <typeinfo>
 
 using namespace std;
+char player = 'X';
 
 /*
 Function Displays the Tic Tac Toe Board Game
 @param matrix[3][3] is thr 2 dimensional array holding each place on the board
 @param size is the size of each array
 */
-void boardGame(int matrix[3][3], int size, int playerMove) 
+void DrawBaord(string matrix[3][3], int size) 
 {
     // Displays the board game
-    int counter = 1;
     for(int i = 0; i < size; i++) {
         for(int j = 0; j < size; j++) {
-            matrix[i][j] = counter++;
             cout << matrix[i][j] << "  ";
-        }
+       }
     cout << endl << endl;
     }
 }
 
-int userInput(int matrix[3][3], int size)
+void updateBaord(string matrix[3][3], int size, int nextMove) 
 {
-    cout << "What your move: ";
-    int player = 0;
-    cin >> player;
-    return player;
+    int i = (nextMove - 1) / 3;
+    int j = (nextMove - 1) % 3;
+    matrix[i][j] = player;
+
+    cout << endl << endl;
+
+    // Displays the board game
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            cout << matrix[i][j] << "  ";
+       }
+    cout << endl << endl;
+    }
+}
+int userInput()
+{
+    int playerMove;
+    cin >> playerMove;
+    return playerMove;
+}
+
+bool winnerCheck(string matrix[3][3], int size) 
+{
+    bool winner = false;
+
+    // Check Rows
+    for(int i = 0; i < size; i++) {
+        if(matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2]) {
+            winner = true;
+            return winner;
+        }
+    }
+
+    // Check Columns
+    for(int j = 0; j < size; j++) {
+        if(matrix[0][j] == matrix[1][j] && matrix[1][j] == matrix[2][j]) {
+            winner = true;
+            return winner;
+        }
+    }
+
+    // Check Left Diagonals
+    if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]) {
+        winner = true;
+        return winner;
+    }
+    //Check Right Diagonals
+    if (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0]) {
+        winner = true;
+        return winner;
+    }
+
+    return winner;
+}
+
+void togglePlayer()
+{
+    if(player == 'X'){
+        cout << "Player O Turn: ";
+        player = 'O';
+    }
+    else {
+        cout << "Player X Turn: "; 
+        player = 'X';
+    }
 }
 
 int main()
 {
     const int size = 3;
-    int matrix[size][size];
+    string matrix[size][size] = { {"1", "2", "3"}, 
+                                  {"4", "5", "6"}, 
+                                  {"7", "8", "9"} };
     cout << endl;
     bool winner = false;
 
-    while(!winner) {
-        // Function that takes in user input
-        int playerMove = userInput(matrix, size);
+    // Function that displays the board
+    DrawBaord(matrix, size);
 
-        // Function that displays the board
-        boardGame(matrix, size, playerMove);
+    while (!winner) {
+        // Function takes in user input
+        int nextMove = userInput();
+
+        // Draw the updated board
+        updateBaord(matrix, size, nextMove);
+
+        // Function that checks if a player has won
+        bool found = winnerCheck(matrix, size);
+        
+        if(found){
+            cout << "We Have A WINNNNNER" << endl;
+        }
+        else{
+            cout << "no Winner yet" << endl;
+        }
+
+        // Function that checks if all slots are filled
+
+        // Flip players
+        togglePlayer();
     }
 
-    // Function that checks if a player has won
 
-    // Function that checks if all slots are filled
 
     return 0;
 }
