4f2d303a2e58cf3233156e19dfd3d22d024e7d62
Anthony Kuregian <Anthonykuregian@yahoo.com>
2019-05-02 09:50:31 -0700

    Need to ask teacher for better clarification

---
+++ Week_Seven/E7_6.cpp
@@ -0,0 +1,24 @@
+// Program that Returns a pointer for the last occurrance
+// of the char "c" in a string
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+/*
+Function that returns the last occurance of the char 'c' in a string
+@param char string[] is the charactor string array
+@param char charactor is the letter that being searched for
+@return will return the pointer value for the location of the char 'c'
+*/
+char *find_last(char string[], char charactor)
+{
+    
+}
+
+int main()
+{
+
+    return 0;
+}
