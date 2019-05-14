20573e55be36c2ea367939c6d1be3886f91118cc
Anthony <AnthonyKuregian@yahoo.com>
2019-05-14 15:46:56 -0700

    Making sure everything is up to date before i pull from my PC

--- Week_Seven/P7_10.cpp
+++ Week_Seven/P7_10.cpp
@@ -3,10 +3,24 @@
 
 using namespace std;
 
+// structures
+struct Course;
+struct Student 
+{
+    string name;
+    vector<Course*> courses;
+};
+
+struct Course 
+{
+    string name;
+    vector<Student*> students;
+};
+
 // void print_students(Student* s) will print the name of the students and all the courses he has taken
 void print_student(Student* s)
 {
-
+    
 }
 
 // void print_course(Course* c) prints the name of the course and all students in the course
@@ -21,22 +35,26 @@ void enroll(Student* s, Course* c)
     
 }
 
-struct Course;
 
-struct Student
-{
-    string name;
-    vector<Course*> courses;
-};
-
-struct Course
-{
-    string name;
-    vector<Student*> students;
-};
 
 int main() 
 {
+    string students[] = {   "Anthony Kuregian",
+                            "John Jones",
+                            "Mary Poppins",
+                            "Mike Smith"    };
+
+    string classes[] = {    "Physics 101",
+                            "CIS 201",
+                            "Calculus 1", 
+                            "Electronics 102" };
+
+    Student *student;
+    Course *course;
+
+    enroll(student, course);
+
+
 
     return 0;
 }
